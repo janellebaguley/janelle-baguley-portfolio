6fda550d3ea5bc833acc2b3af06ccd59cf8b42ee
janellebaguley <nellie.baguley@gmail.com>
2021-03-12 14:33:07 -0700

    updates

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom'
+
 import resume_1 from '../../assets/resume_page1.jpeg'
 import resume_2 from '../../assets/resume_page2.jpeg'
 import './Resume.css'
@@ -8,7 +8,7 @@ import './Resume.css'
 function Resume(){
     return(
         <div className='resume'>      
-            <Link to = 'https://docs.google.com/document/d/18x61qBYH9O8MHvFc8j6-9KbYQnqixP4TIik57MFT5vc/edit'>Resume</Link>
+            
             <div className='container-resume'>
            <img src={resume_1} alt='resume'/>
            <img src={resume_2} alt='resume'/>
