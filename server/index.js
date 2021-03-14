8fca04a73d89e6def629934a027b425c0aee8656
janellebaguley <nellie.baguley@gmail.com>
2021-03-14 09:02:57 -0600

    index and build updates

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const {SERVER_PORT} = process.env
 
 const app = express()
 
-app.use(express.static(__dirname + '/../build'))
+app.use(express.static(`${__dirname}/../build`))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'))
-})
+  })
 
 app.listen(SERVER_PORT, () => console.log(`Running on port: ${SERVER_PORT}`))
